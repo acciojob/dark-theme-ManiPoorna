a93d55edd28ebf6346702aefdd20361566be0eac
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-23 14:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
-
+const main = document.getElementById("app");
+const button = document.getElementById("swap");
+function swapTheme() {
+	if(main.className === "day"){
+		main.className = "night";
+		button.className = "button_night";
+	}	
+	else{
+		main.className = "day";
+		button.className = "button_day";
+	}
+}
